a9aaa50600e31c5bcce60d900b56ac006602b81a
Benoit Antelme <benoitantelme@gmail.com>
2025-07-06 16:51:35 +0100

    Simplifying and using a controller to display the beans

--- src/main/java/com/template/Application.java
+++ src/main/java/com/template/Application.java
@@ -1,31 +1,16 @@
 package com.template;
 
-import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
 
-	static Logger logger = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-		logger.info("Let's inspect the beans provided by Spring Boot:");
-
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			logger.info(beanName);
-		}
+		SpringApplication.run(Application.class, args);
 	}
 	
 }
---
+++ src/main/java/com/template/beans/BeansController.java
@@ -0,0 +1,32 @@
+package com.template.beans;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+public class BeansController {
+
+    Logger logger = LoggerFactory.getLogger(BeansController.class);
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @GetMapping("/beans")
+    public String[] index() {
+        logger.info("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            logger.info(beanName);
+        }
+        return beanNames;
+    }
+
+}
