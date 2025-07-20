ed96d06fb8688cfa7d47402a92db69c0177e340e
Benoit Antelme <benoitantelme@gmail.com>
2025-07-20 10:30:27 +0100

    async module

---
+++ async/src/main/java/com/template/async/AsyncConfig.java
@@ -0,0 +1,44 @@
+package main.java.com.template.async;
+
+import java.util.concurrent.Executor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+  Logger logger = LoggerFactory.getLogger(AsyncConfig.class);
+
+  @Value("${async.core-pool-size:1}")
+  private int corePoolSize;
+
+  @Value("${async.max-pool-size:3}")
+  private int maxPoolSize;
+
+  @Value("${async.queue-capacity:25}")
+  private int queueCapacity;
+
+  @Override
+  public Executor getAsyncExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(corePoolSize);
+    executor.setMaxPoolSize(maxPoolSize);
+    executor.setQueueCapacity(queueCapacity);
+    executor.setThreadNamePrefix("BasicAsync-");
+    executor.initialize();
+    return executor;
+  }
+
+  @Override
+  public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+    return (ex, method, params) -> {
+      logger.error(
+          "Async method {} failed with exception: {}", method.getName(), ex.getMessage(), ex);
+    };
+  }
+}
---
+++ async/src/main/java/com/template/task/SlowTaskController.java
@@ -0,0 +1,17 @@
+package main.java.com.template.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SlowTaskController {
+
+  @Autowired SlowTaskService slowTaskService;
+
+  @GetMapping("/slow")
+  public String slowTaskAsync() {
+    slowTaskService.processAsynchronously();
+    return "Processing Started!";
+  }
+}
