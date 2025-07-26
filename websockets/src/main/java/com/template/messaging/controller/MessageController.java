34ef8d978779f770d1e696513693517243889268
Benoit Antelme <benoitantelme@gmail.com>
2025-07-26 15:33:17 +0100

    web socket module

---
+++ websockets/src/main/java/com/template/messaging/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.template.messaging.controller;
+
+import com.template.messaging.model.Message;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessageController {
+
+  @MessageMapping("/chat")
+  @SendTo("/topic/messages")
+  public Message send(Message message) throws Exception {
+    String time = new SimpleDateFormat("HH:mm").format(new Date());
+    return new Message(message.getFrom(), message.getText(), time);
+  }
+}
