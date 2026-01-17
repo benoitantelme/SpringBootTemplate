b650f6de8df8c5cae704555cf83072eb0096a308
Benoit Antelme <benoitantelme@gmail.com>
2026-01-17 16:17:04 +0000

    Fixed JPA module

--- jpa/src/test/java/com/template/counterparties/CounterpartiesControllerTest.java
+++ jpa/src/test/java/com/template/counterparties/CounterpartiesControllerTest.java
@@ -2,6 +2,7 @@ package com.template.counterparties;
 
 import static org.hamcrest.core.Is.is;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import tools.jackson.databind.ObjectMapper;
@@ -26,27 +26,29 @@ public class CounterpartiesControllerTest {
 
   @Autowired private MockMvc mvc;
 
-  @WithMockUser(roles = "USER", username = "user")
   @Test
   @Order(1)
   public void getCounterparty() throws Exception {
     String CPTY = "BNP";
     mvc.perform(
-            MockMvcRequestBuilders.get("/counterparty/" + CPTY).accept(MediaType.APPLICATION_JSON))
+            MockMvcRequestBuilders.get("/counterparty/" + CPTY)
+                .with(user("user"))
+                .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.name", is(CPTY)));
   }
 
-  @WithMockUser(roles = "USER", username = "user")
   @Test
   @Order(2)
   public void getCounterparties() throws Exception {
-    mvc.perform(MockMvcRequestBuilders.get("/counterparties").accept(MediaType.APPLICATION_JSON))
+    mvc.perform(
+            MockMvcRequestBuilders.get("/counterparties")
+                .with(user("user"))
+                .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.length()", is(3)));
   }
 
-  @WithMockUser(roles = "USER", username = "user")
   @Test
   @Order(3)
   public void postCounterparty() throws Exception {
@@ -54,13 +56,16 @@ public class CounterpartiesControllerTest {
     Counterparty cpty = new Counterparty(TEST);
     mvc.perform(
             MockMvcRequestBuilders.post("/counterparty")
+                .with(user("user"))
                 .with(csrf())
                 .content(new ObjectMapper().writeValueAsString(cpty))
                 .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isCreated());
 
     mvc.perform(
-            MockMvcRequestBuilders.get("/counterparty/" + TEST).accept(MediaType.APPLICATION_JSON))
+            MockMvcRequestBuilders.get("/counterparty/" + TEST)
+                .with(user("user"))
+                .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.name", is(TEST)));
   }
